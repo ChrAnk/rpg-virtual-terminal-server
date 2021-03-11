b024a630982256ba4c178df6c7872636e87248dc
ChrAnk <33005356+ChrAnk@users.noreply.github.com>
2021-03-11 21:10:32 +0100

    Update command_about.php

--- includes/command_about.php
+++ includes/command_about.php
@@ -1,5 +1,5 @@
 <?php
-$data_array[] = "RPG VIRTUAL TERMINAL SERVER VERSION 0.1.2-alpha";
+$data_array[] = "RPG VIRTUAL TERMINAL SERVER VERSION 0.1.3-ALPHA";
 $data_array[] = "";
 $data_array[] = "GLASS TTY VT220 FONT BY VIACHESLAV SLAVINSKY (HTTP://SENSI.ORG/~SVO/GLASSTTY/)";
 ?>
