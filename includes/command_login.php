e7b4c795a8edc91f33c55801c8a0256c6d2b931c
ChrAnk <33005356+ChrAnk@users.noreply.github.com>
2021-03-11 21:03:31 +0100

    Update command_login.php

--- includes/command_login.php
+++ includes/command_login.php
@@ -4,7 +4,7 @@ if(isset($_SESSION['username'])) {
 }
 else if($operative_handler->check_credentials($command_array[1], $command_array[2])) {
 	$_SESSION['username'] = $command_array[1];
-	$_SESSION['user_id'] = strtoupper(substr(hash('sha256', $username . $_SERVER['REMOTE_ADDR']), 8, 16));
+	$_SESSION['user_id'] = strtoupper(substr(hash('sha256', $_SESSION['username'] . $_SERVER['REMOTE_ADDR']), 8, 16));
 	
 	$data_array[] = "ACCESS GRANTED FOR OPERATIVE: " . $_SESSION['username'];
 }
